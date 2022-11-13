f5198cfe622de21b4a00e64277e47cb2e7b7558f
Lev Vereshchagin <mail@vrslev.com>
2022-11-13 17:18:12 +0300

    Add tranpose chart tests

--- chord-chart-py/tests/test_build.py
+++ chord-chart-py/tests/test_build.py
@@ -1,11 +1,21 @@
 import pytest
+from chord_chart import transpose_chart
 from chord_chart._chord_chart import ValidationError, validate_chart
 
 
-def test_validate_chart_ok():
+def test_validate_chart_works():
     assert validate_chart("| C|") == "| C |"
 
 
 def test_validate_chart_throws():
     with pytest.raises(ValidationError, match="bar line should start with stripe: C"):
         validate_chart("C")
+
+
+def test_tranpose_chart_works():
+    assert transpose_chart("| D |", "D", "Gb") == "| Gb |"
+
+
+def test_tranpose_chart_throws():
+    with pytest.raises(ValidationError, match="bar line should start with stripe: C"):
+        transpose_chart("C", "C", "C")
--- chord-chart-ts/index.test.ts
+++ chord-chart-ts/index.test.ts
@@ -1,13 +1,25 @@
-import { describe, expect, test } from "vitest";
-import { validateChart } from ".";
+import { describe, expect, it } from "vitest";
+import { tranposeChart, validateChart } from ".";
 
 describe("validateChart", () => {
-  test("ok", () => {
+  it("works", () => {
     expect(validateChart("| C|")).toEqual("| C |");
   });
 
-  test("throws", () => {
+  it("throws", () => {
     expect(() => validateChart("C")).toThrow(
+      "bar line should start with stripe: C"
+    );
+  });
+});
+
+describe("tranposeChart", () => {
+  it("works", () => {
+    expect(tranposeChart("| D |", "D", "Gb")).toEqual("| Gb |");
+  });
+
+  it("throws", () => {
+    expect(() => tranposeChart("C", "C", "C")).toThrow(
       "bar line should start with stripe: C"
     );
   });
